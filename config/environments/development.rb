89dcb415971df405409736208def98c65f70b3e6
EC2 Default User <ec2-user@ip-10-2-0-112.ap-northeast-1.compute.internal>
2021-12-07 13:06:05 +0000

    config/database.yml 18行目、config/environments/development.rb 3行目、config/puma.rb 13~14行目を修正し、yarn.lockを追加しました

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.hosts << "d8866300ccbc4cc59a12f9c3b289291f.vfs.cloud9.ap-northeast-1.amazonaws.com"
+  config.hosts << "myrails-elb-533735401.ap-northeast-1.elb.amazonaws.com"
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
