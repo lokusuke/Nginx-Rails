9cd46cfdb138dd2a8ba20358234dacf625bbc2ef
EC2 Default User <ec2-user@ip-10-2-0-112.ap-northeast-1.compute.internal>
2021-12-08 12:09:37 +0000

    config/environments/development.rbの3行目を変更

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.hosts << "myrails-elb-533735401.ap-northeast-1.elb.amazonaws.com"
+  config.hosts << "MyRails-ELB-243711501.ap-northeast-1.elb.amazonaws.com"
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
